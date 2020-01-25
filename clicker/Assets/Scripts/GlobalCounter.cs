500e8e94a9b41ecddbb0ae9fe37c2de93cb9fe21
deptster <deepantripathi0000@gmail.com>
2020-01-25 23:39:40 +0530

    update 0.1
    basic script ready

---
+++ clicker/Assets/Scripts/GlobalCounter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GlobalCounter : MonoBehaviour
+{
+    public TextMeshProUGUI CoinDisplay;
+    public TextMeshProUGUI MultiplierDisplay;
+    public int MyCoins;
+    public int MyMultiplier = 1;
+
+    void Update()
+    {
+        CoinDisplay.GetComponent<TextMeshProUGUI>().text = "Coin: " + MyCoins;
+        MultiplierDisplay.GetComponent<TextMeshProUGUI>().text = "Multiplier: x" + MyMultiplier;
+    }
+
+    public void IncreaseCoin()
+    {
+        MyCoins += 1 * MyMultiplier;
+    }
+
+    public void CoinMultiplier()
+    {
+        MyCoins -= 10;
+        MyMultiplier += 1;
+    }
+}
